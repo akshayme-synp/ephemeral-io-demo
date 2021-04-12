2fbd87a10d2344a4c9b9f5ee27c9d87e10ead47b
Akshay <akshayme@synopsys.com>
2021-04-12 12:11:31 -0400

    minor release

--- src/main/java/com/example/security/CustomAuthenticationProvider.java
+++ src/main/java/com/example/security/CustomAuthenticationProvider.java
@@ -67,7 +67,7 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 		User user = new User(listAccounts.get(0).getUsername(), listAccounts.get(0).getPassword(), authList);
 		System.out.println(user);
 		System.out.println(password);
-		System.out.println(authList);
+		System.out.println(listAccounts.get(0).getPassword());
 		return new UsernamePasswordAuthenticationToken(user, password, authList);
 	}
 
